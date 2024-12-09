666641d60d43eb4dd46e9e4720179fd0e9f3ce46
GUI-Cong <cong.gui@student.hamk.fi>
2024-12-09 13:15:07 +0200

    student4 added

--- src/StudentApp.java
+++ src/StudentApp.java
@@ -3,16 +3,19 @@ public class StudentApp {
         Student student1 = new Student("pekka Niemi", "543626");
         Student student2 = new Student("Muntasir Islam", "634765", 20, true);
         Student student3 = new Student ("Yan Tuo" , "569874" , 30 , false);
+        Student student4 = new Student("Gui Cong","623819", 20, true);
         
         student1.bePresent();
         student1.addStudyRecord(15);
         student1.addStudyRecord(25);
         student2.addStudyRecord(20);
         student3.addStudyRecord(30);
+        student4.addStudyRecord(10);
 
         student1.printInfo();
         student2.printInfo();
         student3.printInfo();
+        student4.printInfo();
     }
 } // End of the studentAPP
 
