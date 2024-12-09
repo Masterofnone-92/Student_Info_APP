d030dc3949aecd42b186d2980d88b18882b234a8
YAN TUO <amk1005794@student.hamk.fi>
2024-12-09 11:11:49 +0200

    Added one new student info to the project

--- src/StudentApp.java
+++ src/StudentApp.java
@@ -2,14 +2,17 @@ public class StudentApp {
     public static void main(String[] args) throws Exception {
         Student student1 = new Student("pekka Niemi", "543626");
         Student student2 = new Student("Muntasir Islam", "634765", 20, true);
-
+        Student student3 = new Student ("Yan Tuo" , "569874" , 30 , false);
+        
         student1.bePresent();
         student1.addStudyRecord(15);
         student1.addStudyRecord(25);
         student2.addStudyRecord(20);
+        student3.addStudyRecord(30);
 
         student1.printInfo();
         student2.printInfo();
+        student3.printInfo();
     }
 } // End of the studentAPP
 
