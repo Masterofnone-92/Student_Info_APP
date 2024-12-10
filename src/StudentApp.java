bcd17cb7416e6a2656c67f95209afa5f64e74313
Muntasir Islam <muntasir.islam@yahoo.com>
2024-12-10 12:38:45 +0200

    Added some additional comments

--- src/StudentApp.java
+++ src/StudentApp.java
@@ -1,10 +1,12 @@
 public class StudentApp {
     public static void main(String[] args) throws Exception {
+        // Creating student instances
         Student student1 = new Student("pekka Niemi", "543626");
         Student student2 = new Student("Muntasir Islam", "634765", 20, true);
         Student student3 = new Student("Yan Tuo", "569874", 30, false);
         Student student4 = new Student("Gui Cong", "623819", 20, true);
 
+        // Adding attendance and study records
         student1.bePresent();
         student1.addStudyRecord(15);
         student1.addStudyRecord(25);
@@ -12,6 +14,7 @@ public class StudentApp {
         student3.addStudyRecord(30);
         student4.addStudyRecord(10);
 
+        // Printing student information
         student1.printInfo();
         student2.printInfo();
         student3.printInfo();
@@ -25,6 +28,7 @@ class Student {
     public int credits;
     public boolean attendanceStatus;
 
+    // Default constructor
     public Student() {
         name = "";
         studentNumber = "";
@@ -32,6 +36,7 @@ class Student {
         attendanceStatus = false;
     }
 
+    // Constructor with name and student number
     public Student(String name, String studentNumber) {
         this.name = name;
         this.studentNumber = studentNumber;
@@ -39,6 +44,7 @@ class Student {
         attendanceStatus = false;
     }
 
+    // Constructor with all attributes
     public Student(String name, String studentNumber, int credits, boolean attendanceStatus) {
         this.name = name;
         this.studentNumber = studentNumber;
@@ -46,7 +52,7 @@ class Student {
         this.attendanceStatus = attendanceStatus;
     }
 
-    // Method
+    // Adds credits to the student's record if they are present.
     public void addStudyRecord(int creditAmount) {
         if (attendanceStatus) {
             credits = credits + creditAmount;
@@ -56,22 +62,25 @@ class Student {
         }
     }
 
+    // Prints the credit information of the student.
     public void printCreditinfo() {
         System.out.println("Has  " + credits + " credits");
 
     }
 
-    // Setter
+    // Marks the student as present.
     public void bePresent() {
         attendanceStatus = true;
 
     }
 
+    // Marks the student as absent.
     public void beAbsent() {
         attendanceStatus = false;
 
     }
 
+    // Prints detailed information about the student.
     public void printInfo() {
         System.out.println("Name:  " + name);
         System.out.println("Student number: " + studentNumber);
