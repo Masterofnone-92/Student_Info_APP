959a7b63d703e8f0500754a68c9ea8bfab5bde86
Muntasir Islam <muntasir.islam@yahoo.com>
2024-12-10 12:11:28 +0200

    Add some comments

--- src/StudentApp.java
+++ src/StudentApp.java
@@ -2,9 +2,9 @@ public class StudentApp {
     public static void main(String[] args) throws Exception {
         Student student1 = new Student("pekka Niemi", "543626");
         Student student2 = new Student("Muntasir Islam", "634765", 20, true);
-        Student student3 = new Student ("Yan Tuo" , "569874" , 30 , false);
-        Student student4 = new Student("Gui Cong","623819", 20, true);
-        
+        Student student3 = new Student("Yan Tuo", "569874", 30, false);
+        Student student4 = new Student("Gui Cong", "623819", 20, true);
+
         student1.bePresent();
         student1.addStudyRecord(15);
         student1.addStudyRecord(25);
@@ -46,6 +46,7 @@ class Student {
         this.attendanceStatus = attendanceStatus;
     }
 
+    // Method
     public void addStudyRecord(int creditAmount) {
         if (attendanceStatus) {
             credits = credits + creditAmount;
@@ -60,6 +61,7 @@ class Student {
 
     }
 
+    // Setter
     public void bePresent() {
         attendanceStatus = true;
 
